2cab9b1e34aea3aff515a2eca12293fa2ce5c347
Elitelord <sameeragarwal007@outlook.com>
2025-06-29 22:40:13 -0500

    simpleRockPaperScissors game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,65 @@
+'use client'
+import { copyFile } from "fs";
 import Image from "next/image";
-
+import { useState } from "react";
 export default function Home() {
+  const [choice, setChoice]=useState("");
+  const [resultText, setResultText] = useState("");
+
+  const handleClick=()=>{
+      if(choice!="0"){
+        let compChoice = (Math.floor(Math.random()*3)+1).toString();
+        if(choice!=compChoice){
+          if(choice=="1"&&compChoice=="2"){
+            setResultText("They picked paper, so you lost");
+          }
+          else if(choice=="2"&&compChoice=="1"){
+            setResultText("They picked Rock, so you Win!");
+
+          }
+          else if(choice=="2"&&compChoice=="3"){
+            setResultText("They picked Scissors, so you lost");
+
+          }
+          else if(choice==="3"&&compChoice==="2"){
+            setResultText("They picked Paper, so you Win!");
+          }
+          else if(choice==="1"&&compChoice==="3"){
+            setResultText("They picked Scissors, so you Win!");
+
+          }
+          else if(choice==="3"&&compChoice==="1"){
+            setResultText("They picked Rock, so you lost");
+          }
+        }
+        else{
+          setResultText("They picked the same thing, so you tied!");
+        }
+      }
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <h1 className = "text-lg"> 
-            Hi, This is a Simple Timer App built with NextJS
+            Hi, This is a Simple Rock, Paper, Scissors App built with NextJS
           </h1>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          
+          
+          <select className="text-blue bg-black" onChange={(e)=>setChoice(e.target.value)} defaultValue={"0"}>
+            <option className="text-blue bg-black" value = {"0"} disabled >Select</option>
+            <option className="text-blue bg-black" value = {"1"}>Rock</option>
+            <option className="text-blue bg-black" value = {"2"}>Paper</option>
+            <option className="text-blue bg-black" value = {"3"}>Scissors</option>
+          </select>
+          <button type = "submit" className="bg-blue" onClick={handleClick}>Play</button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+       <p>{resultText}</p>
       </footer>
     </div>
   );
